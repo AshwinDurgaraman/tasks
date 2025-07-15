196e6abe9fa5ff4002b526f8b7846019d0a55639
Thamilvendhan <thamilvendhan@subseltechsolution.com>
2025-07-15 15:22:59 +0530

    Initial commit - added Salesforce project

---
+++ force-app/main/default/lwc/productFilterCustomFilter/productFilterCustomFilter.js
@@ -0,0 +1,101 @@
+import { LightningElement, api, track } from 'lwc';
+
+import getCustomFilter from "@salesforce/apex/ECommerceProductListContainerController.getCustomFilter";
+import getAvailableCustomFilterOptions from "@salesforce/apex/ECommerceProductListContainerController.getAvailableCustomFilterOptions";
+
+export default class ProductFilterCustomFilter extends LightningElement {
+    @api families;
+    @api filterData;
+
+    @track isLoading = false;
+
+    _filterData;
+
+    _availableData = [];
+
+    searchTerm;
+    values = [];
+
+    get avaliableData(){
+        return this._availableData;
+    }
+
+    get enableSearchBoxInput(){
+        return this._filterData?.enableSearchBox;
+    }
+
+    get hasHelpText(){
+        return this._filterData?.help.length > 0;
+    }
+
+    connectedCallback(){
+        if(this.filterData){
+            this._getCustomFilter();
+        }
+    }
+
+    _getCustomFilter(){
+        this.isLoading = true;
+
+        getCustomFilter({recordId: this.filterData.customFilterId})
+        .then(result => {
+            this._filterData = result;
+            this._getAvailableCustomFilterOptions();
+        })
+        .catch(error=>{
+            this.isLoading = false;
+            console.error('ProductFilterCustomFilter error', error.errorMessage, error.stack);
+        })
+    }
+
+    _getAvailableCustomFilterOptions(){
+        getAvailableCustomFilterOptions({
+            recordId: this.filterData.customFilterId
+        })
+        .then(results=>{
+            this._availableData = results;
+        })
+        .catch(error=>{
+            console.error('ProductFilterCustomFilter error', error.errorMessage, error.stack);
+        })
+        .finally(()=>{
+            this.isLoading = false;
+        })
+    }
+
+    _dispatchEvent(){
+        const custEvent = new CustomEvent('customfilterchange', {
+            detail: {
+                filterData: this.filterData,
+                values: this.values,
+                searchTerm: this.searchTerm
+            }
+        });
+
+        this.dispatchEvent(custEvent);
+    }
+
+    handleChangeSearchTerm(event){
+        this.searchTerm = event.target.value;
+
+        this._dispatchEvent();
+    }
+
+    handleChange(){
+        let checboxes = this.template.querySelectorAll('[data-type="customFilter"]');
+
+        this.values = [];
+        if(checboxes){
+            for(const checkbox of checboxes){
+                if(checkbox.checked){
+                    this.values = [
+                        ...this.values,
+                        checkbox.value
+                    ]
+                }
+            }
+        }
+
+        this._dispatchEvent();
+    }
+}
---
+++ force-app/main/default/lwc/productFilterCustomFilters/productFilterCustomFilters.js
@@ -0,0 +1,56 @@
+import { LightningElement, api, track } from 'lwc';
+
+import getCustomFilters from '@salesforce/apex/ECommerceProductListContainerController.getCustomFilters';
+
+export default class ProductFilterCustomFilters extends LightningElement {
+    @api families;
+
+    @track customFilters = [];
+
+    customFiltersValues = [];
+
+    connectedCallback(){
+        this._getCustomFilters();
+    }
+
+    _getCustomFilters(){
+        getCustomFilters({
+            families: this.families
+        })
+        .then(results=>{
+            this.customFilters = results;
+        })
+        .catch(error=>{
+            console.error('ProductFilterCustomFilters _getCustomFilters error', error.errorMessage, error.stack);
+        })
+    }
+
+    handleFilterChange(event){
+        try{
+            const filterData = event.detail.filterData;
+            const indexOfFilter = this.customFiltersValues.findIndex(x => x.customFilterId == filterData.customFilterId);
+
+            if(indexOfFilter >= 0){
+                this.customFiltersValues[indexOfFilter].values = event.detail.values;
+                this.customFiltersValues[indexOfFilter].searchTerm = event.detail.searchTerm;
+            }else{
+                this.customFiltersValues = [
+                    ...this.customFiltersValues,
+                    {
+                        customFilterId: filterData.customFilterId,
+                        values: event.detail.values,
+                        searchTerm: event.detail.searchTerm
+                    }
+                ]
+            }
+
+            const custEvent = new CustomEvent('customfilterschange', {
+                detail: this.customFiltersValues
+            });
+
+            this.dispatchEvent(custEvent);
+        }catch(error){
+            console.error('ProductFilterCustomFilters handleFilterChange error', error.errorMessage, error.stack);
+        }
+    }
+}
